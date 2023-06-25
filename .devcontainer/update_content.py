9a4275bdca999501a0fdd4c16c8e21d27e9b253d
pedantic-curmudgeon <25124961+pedantic-curmudgeon@users.noreply.github.com>
2023-06-25 21:23:10 +0000

    Remove extraneous comments.

--- .devcontainer/update_content.py
+++ .devcontainer/update_content.py
@@ -8,11 +8,9 @@ def main() -> None:
     """Set up dev container's Python development environment."""
     repo_path = Path(__file__).resolve().parents[1]
 
-    # Install requirements.
     requirements_path = repo_path / "requirements.txt"
     run(f"pip install -r {requirements_path}", check=True, shell=True)
 
-    # Install development requirements.
     dev_requirements_path = repo_path / "dev-requirements.txt"
     run(f"pip install -r {dev_requirements_path}", check=True, shell=True)
     run("pre-commit install", check=True, shell=True)
