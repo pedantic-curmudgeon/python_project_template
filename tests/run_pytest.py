bde40959068d3aeb4440cfda3d66881c5a4004fc
pedantic-curmudgeon <25124961+pedantic-curmudgeon@users.noreply.github.com>
2023-08-05 21:50:12 +0000

    Reduce duplication.

--- tests/run_pytest.py
+++ tests/run_pytest.py
@@ -9,6 +9,7 @@ def run_pytest() -> int:
     """Run pytest."""
     tests_folder = Path(__file__).resolve().parent
     repo_folder = tests_folder.parent
+    results_folder = tests_folder / "results"
 
     return run(
         args=(
@@ -17,12 +18,12 @@ def run_pytest() -> int:
             "--numprocesses=auto",
             "--durations=0",
             "--verbosity=2",
-            f"--junitxml={tests_folder}/results/results.xml",
+            f"--junitxml={results_folder}/results.xml",
             f"--cov={repo_folder}",
             f"--cov-config={tests_folder}/.coveragerc",
-            f"--cov-report=html:{tests_folder}/results/htmlcov",
+            f"--cov-report=html:{results_folder}/htmlcov",
             "--cov-report=term",
-            f"--cov-report=xml:{tests_folder}/results/coverage.xml",
+            f"--cov-report=xml:{results_folder}/coverage.xml",
         ),
         check=False,
     ).returncode
